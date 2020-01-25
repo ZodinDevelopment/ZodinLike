3e3d587ec61411115ef1d78cf67670c10def5d2d
Michael Landon <toasterkief@localhost.localdomain>
2020-01-24 23:04:18 -0600

    You can move the '@' character around. Basic functional loop with input handler

---
+++ input_handlers.py
@@ -0,0 +1,25 @@
+import tcod
+
+def handle_keys(key):
+    #movement keys
+    if key.vk == tcod.KEY_UP:
+        return {'move': (0, -1)}
+    elif key.vk == tcod.KEY_DOWN:
+        return {'move': (0, 1)}
+    elif key.vk == tcod.KEY_LEFT:
+        return {'move': (-1, 0)}
+    elif key.vk == tcod.KEY_RIGHT:
+        return {'move': (1, 0)}
+
+    if key.vk == tcod.KEY_ENTER and key.lalt:
+        #Toggle full screen
+        return {'fullscreen': True}
+
+    elif key.vk == tcod.KEY_ESCAPE:
+        #exit
+        return {'exit': True}
+
+    #No key was pressed
+
+    return {}
+
